73bdf5334e31e7b32616fd1f4a08235d915ae289
TANG <tangjunyi456@163.com>
2022-12-10 01:37:04 +0800

    my project

--- assets/script/level_1.ts
+++ assets/script/level_1.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, JsonAsset, Label, find, resources, Prefab, instantiate, Input, view, Animation, tween, Vec3, UITransform, Toggle, EditBox } from 'cc';
+import { _decorator, Component, Node, JsonAsset, Label, find, resources, Prefab, instantiate, Input, view, Animation, tween, Vec3, UITransform, Toggle, EditBox, AudioSource } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('level_1')
@@ -8,32 +8,37 @@ export class level_1 extends Component {
 
     // 
     private FAQData = []
+    // Node of issue
     private issue: Node = null;
+    // Node of answers
     private answers: Node = null;
+    // Prefabs
     private prefabs: Prefab[] = [];
-    // 游戏进度
+    // Step of game
     private gameStep = 0
-    // 荷叶位置
+    // Location of lotus leaf
     private lotusLeafPos = [];
-
+    // Node of frog
     private frog: Node = null;
 
-    // 游戏窗口宽高
+    // Width and height of windows
     private vw = view.getVisibleSize().width;
     private vh = view.getVisibleSize().height;
 
+    // State of timer
     private timerState = false;
     private timer = null
+    // Max of timer
     private timerMax = 30;
 
-    // 开启倒计时
+    // Start the timer
     startTimer(timeMax: number, timeoutFn) {
         let timerNum = timeMax
         let timerFunc = () => {
             if (this.timerState != true) {
                 return
             }
-            // 超时
+            // Overtime
             if (timerNum < 0) {
                 timeoutFn && timeoutFn()
                 return;
@@ -46,57 +51,110 @@ export class level_1 extends Component {
         this.timerState = true;
         timerFunc()
     }
-    // 停止计时
+    // Stop
     stopTimer() {
         this.timerState = false;
         this.timer && clearTimeout(this.timer)
     }
 
-    // 重新开始
+    // Restart
     reInit() {
-        // 关闭问答窗口 
+        // Close the window 
         find("Canvas/Camera/UI/FAQ_Window").active = false
-        // 显示错误弹窗并重新开始
+        // Error
         find("Canvas/Camera/UI/FAQ_error").active = true;
-        // 清理进度 
+        // Clear the step 
         this.gameStep = 0
-        // 回到起点
+        // Frog go back to the initial point
         this.frog.setPosition(-450, 0, 0);
         find("Canvas/Camera").setPosition(0, 0, 0)
-        // 
+        // Stop timer
         this.stopTimer()
-
+        // Delay closing the answer error window
         setTimeout(() => {
             find("Canvas/Camera/UI/FAQ_error").active = false;
-            find("Canvas/Camera/UI/FAQ_Window").active = true
-            this.startTimer(this.timerMax, () => {
-                this.reInit();
-            })
+            this.showFAQ(0)
         }, 1000);
     }
 
+    // Stop all audio
+    stopAllFaqAudio() {
+        let _sounds = find("Canvas/sound").children
+        for (let _s of _sounds) {
+            _s.getComponent(AudioSource).stop();
+        }
+    }
 
+    // Show FAQ
+    showFAQ(index) {
+        // Stop all audios
+        this.stopAllFaqAudio()
+        // Hide the imgs of FAQ
+        let _FAQ_imgs = find("Canvas/Camera/UI/FAQ_Window/FAQ_img").children
+        for (let _s of _FAQ_imgs) {
+            _s.active = false;
+        }
+        // Remove all the options
+        this.answers.removeAllChildren();
+        // Set the questions
+        this.issue.getComponent(Label).string = this.FAQData[index]["issue"]
+        let faqIndex = this.FAQData[index]["index"]
+        // Set the answers
+        if (this.FAQData[index]["type"] == "select") {
+            for (let i = 0; i < this.FAQData[index]["options"].length; i++) {
+                const el = this.FAQData[index]["options"][i];
+                // instantiate the prefabs
+                let _node = instantiate(this.prefabs["answers_item"])
+                _node.setPosition(0, i * -40, 0);
+                _node.getChildByName("Label").getComponent(Label).string = el
+                // UI->answers
+                this.answers.addChild(_node)
+                _node.getChildByName("Label").on(Input.EventType.MOUSE_DOWN, () => {
+                    _node.getComponent(Toggle).setIsCheckedWithoutNotify(true);
+                })
+            }
+            this.answers.active = true;
+            find("Canvas/Camera/UI/FAQ_Window/EditBox").active = false
+        } else {
+            this.answers.active = false;
+            find("Canvas/Camera/UI/FAQ_Window/EditBox").active = true
+        }
+        // Show the imgs of FAQ
+        find("Canvas/Camera/UI/FAQ_Window/FAQ_img/" + faqIndex).active = true
+
+        // Play the audio
+        find("Canvas/Camera/UI/FAQ_Window/sound").on(Input.EventType.MOUSE_DOWN, () => {
+            find("Canvas/sound/" + faqIndex).getComponent(AudioSource).play()
+        })
+        // Active the window 
+        find("Canvas/Camera/UI/FAQ_Window").active = true
+        // Start the timer
+        this.startTimer(this.timerMax, () => {
+            this.reInit()
+        })
+    }
+
+    // Start
     async start() {
         let _this = this;
-        // 获取到 FAQ Json 数据
+        // Get FAQ Json data
         const jsonData: object = this.FAQ.json!;
         this.FAQData = jsonData["datas"];
         // 
         this.issue = find("Canvas/Camera/UI/FAQ_Window/issue")
         this.answers = find("Canvas/Camera/UI/FAQ_Window/answers")
-
         this.frog = find("Canvas/scene/frog")
 
 
 
-        // 预制体资源路径
+        // The path of the prefabs
         const prefabsPath = {
             "answers_item": "FAQ/answers_item",
             "LotusLeaf": "prefab/LotusLeaf",
             "LotusLeaf2": "prefab/LotusLeaf2",
         }
 
-        // 使用 promise 同步加载资源
+        // Use promise to load the resources
         for (const key in prefabsPath) {
             await new Promise((okFunc, errFunc) => {
                 resources.load(prefabsPath[key], Prefab, (err, p) => {
@@ -110,10 +168,9 @@ export class level_1 extends Component {
             })
         }
 
-
-        // 生成荷叶 
+        // Generate the lotus leaf 
         for (let i = 0; i < this.FAQData.length; i++) {
-            // 随机荷叶类型
+            // Random the type of lotus Leaf
             let LotusLeafName = "LotusLeaf"
             let _x = i * 200 + (this.vw / 2 * -1 + 50)
             let _y = Math.random() * 50
@@ -125,46 +182,25 @@ export class level_1 extends Component {
             }
             _y *= -1
             this.lotusLeafPos.push({ x: _x, y: _y, type: LotusLeafName })
-            // 实例化预制体
+            // Instantiate the prefabs
             let _node = instantiate(this.prefabs[LotusLeafName])
             _node.setPosition(_x, _y, 0);
             find("Canvas/scene/LotusLeafs").addChild(_node)
         }
+        // Show the first question
+        this.showFAQ(0);
 
-
-        // 设置问题
-        this.issue.getComponent(Label).string = this.FAQData[0]["issue"]
-        // 设置答案
-        if (this.FAQData[0]["type"] == "select") {
-            for (let i = 0; i < this.FAQData[0]["options"].length; i++) {
-                const el = this.FAQData[0]["options"][i];
-                // 实例化预制体
-                let _node = instantiate(this.prefabs["answers_item"])
-                _node.setPosition(0, i * -40, 0);
-                _node.getChildByName("Label").getComponent(Label).string = el
-                _node.on(Input.EventType.MOUSE_DOWN, () => {
-                    _node.getComponent(Toggle).setIsCheckedWithoutNotify(true);
-                })
-                // 添加到 UI->answers
-                this.answers.addChild(_node)
-            }
-            this.answers.active = true;
-            find("Canvas/Camera/UI/FAQ_Window/EditBox").active = false
-        } else {
-            this.answers.active = false;
-
-            find("Canvas/Camera/UI/FAQ_Window/EditBox").active = true
-
-        }
-        // 问答窗口的提交按钮，按下 事件
+        // Submit
         find("Canvas/Camera/UI/FAQ_Window/Button").on(Input.EventType.MOUSE_DOWN, () => {
+            // Hide the windows
             find("Canvas/Camera/UI/FAQ_Window").active = false
-
-            // 判断答案 是否正确
+            // Stop all audios
+            this.stopAllFaqAudio()
+            // check the answer
             let _answersRet = false;
             let _faq = this.FAQData[this.gameStep]
             switch (_faq["type"]) {
-                // 选择题 
+                // Select 
                 case "select":
                     for (let index = 0; index < this.answers.children.length; index++) {
                         if (this.answers.children[index].getComponent(Toggle).isChecked) {
@@ -178,78 +214,43 @@ export class level_1 extends Component {
 
                     }
                     break;
-                // 简答题
-                case "text":
-                    console.log(find("Canvas/Camera/UI/FAQ_Window/EditBox").getComponent(EditBox).string);
-                    
-                    _answersRet = true;
-                    break
-                default:
-                    break;
             }
             if (_answersRet) {
-                // 回答正确停止计时 
+                // Stop timer 
                 this.stopTimer()
-                // 进度加一
+                // Step++
                 this.gameStep++;
                 if (this.gameStep >= this.FAQData.length) {
-                    alert("答题完毕，你通过了测试")
+                    alert("Congratulations, you successfully reach the other shore")
                     return
                 }
-                // 跳过一个荷叶，
+                // Jump
                 let _p = [this.lotusLeafPos[this.gameStep].x - this.frog.position.x, this.lotusLeafPos[this.gameStep].y - this.frog.position.y]
                 let _h = 20
                 if (this.lotusLeafPos[this.gameStep].type == "LotusLeaf2") {
                     _h = 100
                 }
-                // 缓动的时长
+                // The duration
                 let tweenDuration: number = 0.3;
                 tween(this.frog)
                     .to(tweenDuration, { position: new Vec3(this.lotusLeafPos[this.gameStep].x - _p[0] / 2, this.lotusLeafPos[this.gameStep].y + 50 + _h, 0) })
                     .to(tweenDuration, { position: new Vec3(this.lotusLeafPos[this.gameStep].x, this.lotusLeafPos[this.gameStep].y + _h, 0) }, {
                         onComplete: () => {
-                            // 播放荷叶动画
+                            // Play the animation
                             find("Canvas/scene/LotusLeafs").children[this.gameStep].getComponent(Animation).play()
-                            // 如果青蛙移动到边界则移动相机
+                            // Move the camera
                             let _cam = find("Canvas/Camera")
                             if ((this.frog.position.x > (_cam.position.x - this.vw / 2) + this.vw * 0.8)) {
-                                // 如果不是最后一题 就移动相机
+                                // Move the camera
                                 if (this.gameStep + 1 < this.FAQData.length) {
                                     tween(find("Canvas/Camera"))
                                         .by(1, { position: new Vec3(this.vw * 0.25, 0, 0) })
                                         .start()
                                 }
                             }
-                            // 开启下一个选项窗口
+                            // Show the next window
                             setTimeout(() => {
-                                this.answers.removeAllChildren();
-                                // 设置问题
-                                this.issue.getComponent(Label).string = this.FAQData[this.gameStep]["issue"]
-                                // 设置答案
-                                if (this.FAQData[this.gameStep]["type"] == "select") {
-                                    // 选择题
-                                    for (let i = 0; i < this.FAQData[this.gameStep]["options"].length; i++) {
-                                        const el = this.FAQData[this.gameStep]["options"][i];
-                                        // 实例化预制体
-                                        let _node = instantiate(this.prefabs["answers_item"])
-                                        _node.setPosition(0, i * -40, 0);
-                                        _node.getChildByName("Label").getComponent(Label).string = el
-                                        // 添加到 UI->answers
-                                        this.answers.addChild(_node)
-                                    }
-                                    this.answers.active = true;
-                                    find("Canvas/Camera/UI/FAQ_Window/EditBox").active = false
-                                } else {
-                                    // 简答题
-                                    this.answers.active = false;
-                                    find("Canvas/Camera/UI/FAQ_Window/EditBox").active = true
-                                }
-                                // 激活问答窗
-                                find("Canvas/Camera/UI/FAQ_Window").active = true
-                                // 开启答题计时器
-                                this.startTimer(this.timerMax, () => {
-                                    this.reInit()
-                                })
+                                this.showFAQ(this.gameStep);
                             }, 666);
                         }
                     })
@@ -259,14 +260,6 @@ export class level_1 extends Component {
                 this.reInit()
             }
         })
-
-        // 激活问答窗
-        find("Canvas/Camera/UI/FAQ_Window").active = true
-        // 开启答题计时器
-        this.startTimer(this.timerMax, () => {
-            this.reInit()
-        })
-
     }
 
     update(deltaTime: number) {
